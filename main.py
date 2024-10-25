638f5c8f4535001cd0349407a6c8a8abe1691cc9
Hemanth Vadde <140707363+hemanth-vadde-official@users.noreply.github.com>
2024-10-26 02:01:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,30 +5,39 @@ from fastapi.templating import Jinja2Templates
 import uvicorn
 
 
+from fastapi import FastAPI
+
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    print('Request for index page received')
-    return templates.TemplateResponse('index.html', {"request": request})
+@app.get("/")
+async def read_root():
+    return {"Hello, World!"}
 
-@app.get('/favicon.ico')
-async def favicon():
-    file_name = 'favicon.ico'
-    file_path = './static/' + file_name
-    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
 
-@app.post('/hello', response_class=HTMLResponse)
-async def hello(request: Request, name: str = Form(...)):
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+# app = FastAPI()
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+# templates = Jinja2Templates(directory="templates")
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000)
+# @app.get("/", response_class=HTMLResponse)
+# async def index(request: Request):
+#     print('Request for index page received')
+#     return templates.TemplateResponse('index.html', {"request": request})
+
+# @app.get('/favicon.ico')
+# async def favicon():
+#     file_name = 'favicon.ico'
+#     file_path = './static/' + file_name
+#     return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
+
+# @app.post('/hello', response_class=HTMLResponse)
+# async def hello(request: Request, name: str = Form(...)):
+#     if name:
+#         print('Request for hello page received with name=%s' % name)
+#         return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
+#     else:
+#         print('Request for hello page received with no name or blank name -- redirecting')
+#         return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+
+# if __name__ == '__main__':
+#     uvicorn.run('main:app', host='0.0.0.0', port=8000)
 
